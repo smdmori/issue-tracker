d3d71b0514dda73644c1b289bb5910d44a5fa24f
smdmori <mortezasamadi1371@gmail.com>
2023-10-16 19:35:22 +0330

    Add Loading Skeletons

---
+++ app/components/IssuesAction.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
+
+const IssuesAction = () => {
+  return (
+    <Button><Link href={'/issues/new'}>New Issue</Link></Button>
+  )
+}
+
+export default IssuesAction
