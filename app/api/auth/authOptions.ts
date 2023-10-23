bf62e68fcc4d10f4e63673693eefda941f8d6e4b
smdmori <mortezasamadi1371@gmail.com>
2023-10-23 16:15:12 +0330

    Secure the Application

---
+++ app/api/auth/authOptions.ts
@@ -0,0 +1,24 @@
+import prisma from "@/prisma/client";
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import {NextAuthOptions} from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+};
+
+export default authOptions;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,15 +1,18 @@
 import prisma from "@/prisma/client"
 import { Box, Flex, Grid } from "@radix-ui/themes"
+import { getServerSession } from "next-auth"
 import { notFound } from "next/navigation"
+import DeleteIssueButton from "./DeleteIssueButton"
 import EditIssueButton from "./EditIssueButton"
 import IssueDetails from "./IssueDetails"
-import DeleteIssueButton from "./DeleteIssueButton"
+import authOptions from "@/app/api/auth/authOptions"
 
 interface Props {
   params: { id: string }
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions)
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) }
   })
@@ -21,10 +24,10 @@ const IssueDetailPage = async ({ params }: Props) => {
       <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
-      <Flex direction="column" gap={'4'}>
+      {session && <Flex direction="column" gap={'4'}>
         <EditIssueButton issueId={issue.id} />
         <DeleteIssueButton issueId={issue.id} />
-      </Flex>
+      </Flex>}
     </Grid>
   )
 }
