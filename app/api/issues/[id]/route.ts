92e48220ac62d8e306f34d4131d4a6df3aa0b439
smdmori <mortezasamadi1371@gmail.com>
2023-10-24 17:34:14 +0330

    Enhance the API to assign issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,4 +1,4 @@
-import {issueSchema} from "@/app/validationSchema";
+import {patchIssueSchema} from "@/app/validationSchema";
 import prisma from "@/prisma/client";
 import {getServerSession} from "next-auth";
 import {NextRequest, NextResponse} from "next/server";
@@ -12,9 +12,18 @@ export async function PATCH(
   if (!session) return NextResponse.json({}, {status: 401});
 
   const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const validation = patchIssueSchema.safeParse(body);
   if (!validation.success) {
     return NextResponse.json(validation.error.format(), {status: 400});
+  }
+
+  const {assignedToUserId, title, description} = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: {id: assignedToUserId},
+    });
+
+    if (!user) return NextResponse.json({error: "Invalid user"}, {status: 400});
   }
 
   const issue = await prisma.issue.findUnique({
@@ -28,8 +37,9 @@ export async function PATCH(
   const updatedIssue = await prisma.issue.update({
     where: {id: issue.id},
     data: {
-      title: body.title,
-      description: body.description,
+      title,
+      description,
+      assignedToUserId,
     },
   });
 
