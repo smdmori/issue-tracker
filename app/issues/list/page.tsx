6b6fa66bb1e5f50e42331226c454de664ff1ff5f
smdmori <mortezasamadi1371@gmail.com>
2023-10-26 11:56:28 +0330

    Make Columns ascending Sortable

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,14 +1,23 @@
 import prisma from "@/prisma/client"
 import { Table } from "@radix-ui/themes"
 import { IssueStatusBadge, Link } from "../../components"
+import NextLink from "next/link"
 import IssueActions from "./IssueActions"
-import { Status } from "@prisma/client"
+import { Issue, Status } from "@prisma/client"
+import { ArrowUpIcon } from "@radix-ui/react-icons"
 
 interface Props {
   searchParams: {
     status: Status
-  }
+    orderBy: keyof Issue
+  },
 }
+
+const columns: { label: string, value: keyof Issue, className?: string }[] = [
+  { label: 'Issue', value: "title" },
+  { label: 'Status', value: "status", className: "hidden md:table-cell" },
+  { label: 'Created', value: "createdAt", className: "hidden md:table-cell" },
+]
 
 const IssuesPage = async ({ searchParams }: Props) => {
   const statuses = Object.values(Status)
@@ -22,9 +31,16 @@ const IssuesPage = async ({ searchParams }: Props) => {
       <Table.Root variant="surface" className="mt-5">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className="hidden md:table-cell">Created</Table.ColumnHeaderCell>
+            {columns.map(column => (
+              <Table.ColumnHeaderCell
+                key={column.value}
+              >
+                <NextLink href={{
+                  query: { ...searchParams, orderBy: column.value }
+                }}>{column.label}</NextLink>
+                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline" />}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
