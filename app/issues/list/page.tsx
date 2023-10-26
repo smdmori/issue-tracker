a256152c9ebbf24ff499592cf1d2104905ede597
smdmori <mortezasamadi1371@gmail.com>
2023-10-26 18:42:26 +0330

    Sort Issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -22,8 +22,16 @@ const columns: { label: string, value: keyof Issue, className?: string }[] = [
 const IssuesPage = async ({ searchParams }: Props) => {
   const statuses = Object.values(Status)
   const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
+  const orderBy = columns
+    .map(column => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: 'asc' }
+    : undefined
 
-  const issues = await prisma.issue.findMany({ where: { status } })
+  const issues = await prisma.issue.findMany({
+    where: { status },
+    orderBy
+  })
 
   return (
     <div>
